9f8c1848dec38e4495bd241934e4c533f053866b
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-04 23:14:00 +0100

    copied css across and started navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import styles from "../css/navbar.module.css"
+import { FaAlignRight, FaTwitter } from "react-icons/fa"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import logo from "../images/logo.svg"
 
 const NavBar = () => {
+  const [isOpen, setNav] = useState(false)
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+  console.log(isOpen)
+
   return (
-    <div>
-      <h1>This is navbar</h1>
-    </div>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo" />
+          <button type="button" className={styles.logoBtn} onClick={toggleNav}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+      </div>{" "}
+    </nav>
   )
 }
 
---
+++ src/constants/social-icons.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { FaFacebook, FaTwitterSquare, FaMedium } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com",
+  },
+  {
+    icon: <FaMedium />,
+    url: "https://medium.com",
+  },
+]
