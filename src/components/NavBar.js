443b03c0ca908f86d78eafa8585e3c5acfece9ec
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-05 21:21:27 +0100

    added footer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
 import styles from "../css/navbar.module.css"
-import { FaAlignRight, FaTwitter } from "react-icons/fa"
+import { FaAlignRight } from "react-icons/fa"
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
 import logo from "../images/logo.svg"
@@ -22,7 +22,36 @@ const NavBar = () => {
             <FaAlignRight className={styles.logoIcon} />
           </button>
         </div>
-      </div>{" "}
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            )
+          })}
+        </ul>
+        <div className={styles.navSocialLinks}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a
+                key={index}
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {item.icon}
+              </a>
+            )
+          })}
+        </div>
+      </div>
     </nav>
   )
 }
