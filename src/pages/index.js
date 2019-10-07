d83b46092f1645755fdf25761a9569d05ba284e7
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-07 15:10:43 +0100

    added gatsby backround image plugin and set up the styled hero component to use graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
+import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
 
-export default () => (
-  <>
-    <Layout>
-      <SimpleHero>
-        <Banner
-          title="Continue Exploring"
-          info="Incididunt fugiat do cillum consectetur proident id dolor pariatur. Ea ea id non nisi consequat. Id minim non ipsum incididunt amet adipisicing ea culpa ullamco qui amet do occaecat."
-        >
-          <Link to="/tours" className="btn-white">
-            explore tours
-          </Link>
-        </Banner>
-      </SimpleHero>
-      <About />
-      <Services />
-    </Layout>
-  </>
+export default ({ data }) => (
+  <Layout>
+    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
+      <Banner
+        title="Explore the world fast"
+        info="Incididunt fugiat do cillum consectetur proident id dolor pariatur. Ea ea id non nisi consequat. Id minim non ipsum incididunt amet adipisicing ea culpa ullamco qui amet do occaecat."
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </StyledHero>
+    <About />
+    <Services />
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
