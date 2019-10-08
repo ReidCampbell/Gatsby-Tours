d8865fbfd71b58b4a2fc3ca3cc9812617b3a9827
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-08 20:51:03 +0100

    updated metadata

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -9,6 +9,7 @@ const getData = graphql`
         siteTitle: title
         description: description
         author
+        image
         siteUrl
         twitterUsername
       }
@@ -20,24 +21,31 @@ const SEO = ({ title, description }) => {
   const { site } = useStaticQuery(getData)
   const {
     siteTitle,
-    siteDesc,
-    siteUrl,
+    author,
     image,
+    siteUrl,
     twitterUsername,
   } = site.siteMetadata
 
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
-      <meta name="description" content={description || siteDesc} />
+      <meta name="description" content="Travel website build with GatsbyJS" />
       <meta name="image" content={image} />
+      <meta name="author" content={author} />
       <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={siteDesc} />
+      <meta
+        name="twitter:description"
+        content="Travel website build with GatsbyJS"
+      />
       <meta name="twitter:username" content={twitterUsername} />
       <meta name="twitter:image" content={`${siteUrl}${image}`} />
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={siteTitle} />
-      <meta property="og:description" content={siteDesc} />
+      <meta
+        property="og:description"
+        content="Travel website build with GatsbyJS"
+      />
       <meta property="og:image" content={`${siteUrl}${image}`} />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="300" />
