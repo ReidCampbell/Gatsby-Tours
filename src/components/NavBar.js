b0b984a085fbd42fdf32f110db09b8bdcc81c1a3
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-08 16:51:13 +0100

    fixed some styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import styles from "../css/navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
+
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
 import logo from "../images/logo.svg"
+import styles from "../css/navbar.module.css"
 
 const NavBar = () => {
   const [isOpen, setNav] = useState(false)
   const toggleNav = () => {
     setNav(isOpen => !isOpen)
   }
-  console.log(isOpen)
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navCenter}>
         <div className={styles.navHeader}>
-          <img src={logo} alt="backroads logo" />
+          <AniLink cover direction="up" duration={1} bg="#663399" to="/">
+            <img src={logo} alt="gatsby tours logo" />
+          </AniLink>
           <button type="button" className={styles.logoBtn} onClick={toggleNav}>
             <FaAlignRight className={styles.logoIcon} />
           </button>
